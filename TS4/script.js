cca916053c1ee3ed6bd29950d7227d7d83b1cb42
KimShinDA <delamenkimshin@gmail.com>
2025-03-26 02:01:50 +0800

    Updated pics and function 03/26/25

--- TS4/script.js
+++ TS4/script.js
@@ -1,46 +1,46 @@
 const restaurantData = {
     gabbel: {
-        pic: "https://i.pinimg.com/736x/16/8e/c1/168ec1834c2717bb69f19fa29d81324f.jpg",
+        pic: "https://i.pinimg.com/736x/c6/d3/90/c6d390d21bfa4479f8348993be481948.jpg",
         name: "Gabbel Loffel",
         address: "235 Glendale Ave, Meridien City",
         phone: "490 49 4000",
         website: "GANDG.RES",
         category: "Swiss",
         hours: "10AM - 10PM",
-        price: "$120",
+        price: "$$$",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet.\n\nMauris ante ligula, facilisis sed ornare eu, lobortis in odio. Praesent convallis urna a lacus interdum ut hendrerit risus congue. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac."
     },
     gary: {
-        pic: "https://i.pinimg.com/474x/33/70/1b/33701b997759e440311f03edd7fb26d9.jpg",
+        pic: "https://i.pinimg.com/736x/48/b2/9d/48b29dae8b93a069efb3cb202c8a84a4.jpg",
         name: "GARY GARI",
         address: "76 No. Highland Ave, Meridien City",
         phone: "490 24 6709",
         website: "GANDG.RES",
         category: "Japanese",
         hours: "5PM - 10PM",
-        price: "$90",
+        price: "$$$",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa.\n\nFusce luctus vestibulum augue ut aliquet. Mauris ante ligula, facilisis sed ornare eu, lobortis in odio. Praesent convallis urna a lacus interdum ut hendrerit risus congue."
     },
     ilpiatto: {
-        pic: "https://i.pinimg.com/736x/66/5b/f9/665bf9454df91c08ec91a0e7da09ace3.jpg",
+        pic: "https://i.pinimg.com/736x/44/0e/70/440e702aacce1b0c35cfb2d71967dad6.jpg",
         name: "IL PIATTO",
         address: "1213 Shoutout Way, Meridien City",
         phone: "490 42 1204",
         website: "ILPIATTO.RES",
         category: "Italian",
         hours: "11AM - 11PM",
-        price: "$110",
+        price: "$$$",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula venenatis dolor.\n\nMaecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Mauris ante ligula, facilisis sed ornare eu, lobortis in odio. Praesent convallis urna a lacus interdum ut hendrerit risus congue."
     },
     pierre: {
-        pic: "https://i.pinimg.com/736x/aa/f5/24/aaf524956d9ea6e589e19561b0b7015d.jpg",
+        pic: "https://i.pinimg.com/736x/ea/4c/88/ea4c889b2288b729df6b5ec7b7359a9f.jpg",
         name: "PIERRE PLATTERS",
         address: "68 8th Ave, Meridien City",
         phone: "490 69 3080",
         website: "PIERREPRES",
         category: "Fusion",
         hours: "11AM - 11PM",
-        price: "$120",
+        price: "$$$",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula venenatis dolor.\n\nMaecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Mauris ante ligula, facilisis sed ornare eu, lobortis in odio. Praesent convallis urna a lacus interdum ut hendrerit risus congue."
     }
 };
@@ -70,11 +70,18 @@ function showDetails(restaurant) {
     // Show popup
     document.getElementById("restaurant-popup").style.display = "block";
     document.body.style.overflow = "hidden";
+
+    // Hide restaurants container
+    document.querySelector(".restaurants").style.display = "none";
+
 }
 
 function closePopup() {
     document.getElementById("restaurant-popup").style.display = "none";
     document.body.style.overflow = "auto";
+
+     // Show restaurants container again
+     document.querySelector(".restaurants").style.display = "flex";
 }
 
 // Attach click events to restaurant cards
